aa101083c50ea8f83f00ebec32283bc75ae751de
Rhyan Vargas <rhyanv1@gmail.com>
2022-03-21 19:47:47 -0500

    hardcode progress bar and style

--- docs/scripts/vesting.js
+++ docs/scripts/vesting.js
@@ -1,4 +1,10 @@
+const progressMeter = document.querySelector(".progress-fill");
+
 // convert time
 const toSeconds = (_time) => {
 
+}
+
+function updateProgress(value) {
+    progressMeter.style.width = `${value}%`;
 }
