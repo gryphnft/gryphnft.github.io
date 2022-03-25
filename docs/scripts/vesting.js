2f29bec6706dcd9bc8fef65720240f01cb93983f
Rhyan Vargas <rhyanv1@gmail.com>
2022-03-25 15:24:22 -0500

    integrate buy function to button and add custom messages during process

--- docs/scripts/vesting.js
+++ docs/scripts/vesting.js
@@ -4,7 +4,7 @@
     const connected = async function (newstate) {
         Object.assign(state, newstate, { connected: true })
         blockapi.notify('success', 'Wallet connected')
-        console.log(state.account);
+        console.log("user connected address", state.account);
     }
     const disconnected = function (e) {
         if (e?.message) {
@@ -14,12 +14,18 @@
         }
     }
     const contract = await blockapi.contract('vesting');
+    const contractAddress = contract._address;
+    console.log("contract address", contractAddress);
 
+    // GLOBAL VARIABLES
+    const loadingBox = document.getElementById
+        ('loading-box')
+    const tokenForm = document.getElementById('token-sale-form')
 
     // COUNTDOWN TIMER
     const countdown = document.querySelector(".countdown");
     const expiredMessage = document.getElementById("countdown-expired-message");
-    const endSaleDate = new Date("March 25, 2022 15:37:25").getTime();
+    const endSaleDate = new Date("March 26, 2022 15:37:25").getTime();
     // Update the count down every 1 second
     let x = setInterval(function () {
         // Get today's date and time
@@ -39,8 +45,13 @@
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            expiredMessage.style.display = "flex";
-            expiredMessage.innerHTML = `<h2 class="small">Token Sale has ended</h2>`;
+            tokenForm.style.display = 'none'
+            loadingBox.style.display = 'block'
+            loadingBox.innerHTML = `
+        <h3 class="text-center">Token sale has ended</h3>
+        `
+            // expiredMessage.style.display = "flex";
+            // expiredMessage.innerHTML = `<h2 class="small">Token Sale has ended</h2>`;
         } else {
             countdown.style.display = "flex";
         }
@@ -65,18 +76,21 @@
     const orderButton = document.getElementById('order-button');
     const ethTotalContainer = document.querySelector(".total-container");
     const ethTotalValue = document.getElementById("eth-total-value");
+    const currentTokenPrice = await blockapi.read(contract, 'currentTokenPrice');
     const toolTipContainer = document.getElementById("tooltip-container");
     const toolTipMultiplier = document.getElementById("tooltip-multiplier");
-    const currentTokenPrice = await blockapi.read(contract, 'currentTokenPrice');
     const purchaseInput = document.getElementById("amount");
+    let purchaseAmount; // number of GRYPH Tokens
+    let ehtAmount; // total price in ETH
+    let oneGryphToEther; // price of 1 gryph in ETH
     async function calculateToEth(e) {
-        let purchaseAmount = e.target.value;
+        purchaseAmount = e.target.value;
         // Convert gryph price(wei) into ether
-        let etherValueOfGryph = await blockapi.toEther(currentTokenPrice, 'string')
-        let ethTotal = (etherValueOfGryph * purchaseAmount).toFixed(6);
+        oneGryphToEther = await blockapi.toEther(currentTokenPrice, 'string')
+        ehtAmount = (oneGryphToEther * purchaseAmount).toFixed(6);
         if (purchaseAmount) {
             ethTotalContainer.style.display = 'flex';
-            ethTotalValue.textContent = `${ethTotal
+            ethTotalValue.textContent = `${ehtAmount
                 } ETH`;
         } else {
             ethTotalContainer.style.display = 'none';
@@ -87,17 +101,6 @@
             evt.preventDefault();
         }
     }
-    // EVENTS
-    purchaseInput.addEventListener('input', function (e) {
-        if (parseInt(e.target.value) == 0
-            || (e.value?.length == undefined || e.value.length == 0)) {
-            orderButton.disabled = true
-        } else {
-            orderButton.disabled = false
-        }
-    })
-    purchaseInput.addEventListener('keypress', numbersOnly)
-    purchaseInput.addEventListener('keyup', calculateToEth)
 
 
     // BUY TOKENS
@@ -106,12 +109,79 @@
             blockapi.notify('error', 'Please enter a purchase amount')
             return false
         }
-        alert(purchaseInput.value);
+        // Ensure Connection to
+        if (!state.account) {
+            blockapi.connect(blockmetadata, async function (newstate) {
+                await connected(newstate)
+            }, disconnected)
+            return false
+        }
 
+        let rpc;
+        payableAmount = currentTokenPrice * purchaseAmount;
+        try {
+            rpc = blockapi.send(
+                contract,
+                state.account,
+                'buy(address,uint256)',
+                payableAmount, // value
+                state.account,
+                purchaseAmount // amount of gryph tokens
+            )
+        } catch (e) {
+            blockapi.notify('error', e.message)
+            return false;
+        }
+
+
+        rpc.on('transactionHash', function (hash) {
+            // Hide TokenForm and display Processing message 
+            let loadingMessage = `Do not close this window...`
+            tokenForm.style.display = 'none'
+            loadingBox.style.display = 'block'
+            loadingBox.innerHTML = `
+            <h3 class="text-center">${loadingMessage}</h3>
+            <span class="small text-center">Window will automaitcally refresh when transaction is confirmed...</span>`
+
+            blockapi.notify(
+                'success',
+                `Transaction started for purchase of "${purchaseAmount} $GRYPH": <a href="${blockmetadata.chain_scan}/tx/${hash}" target="_blank">
+          etherscan.com
+        </a>`,
+                1000000
+            )
+        })
+        rpc.on('receipt', function (receipt) {
+            blockapi.notify(
+                'success',
+                `Success! You purchased "${purchaseAmount} $GRYPH for ${ehtAmount} ETH" View details: <a href="${blockmetadata.chain_scan}/tx/${receipt.transactionHash}" target="_blank">
+          etherscan.com</a>. Browser will refresh soon...`
+                , 1000000)
+        })
+
+        try {
+            await rpc
+            setTimeout(function () {
+                location.reload()
+            }, 10000);
+        } catch (e) {
+            blockapi.notify('error', e.message)
+        }
+        return false
     }
+    // EVENTS
+    purchaseInput.addEventListener('input', function (e) {
+        if (parseInt(e.target.value) == 0 ||
+            (e.target.value?.length == undefined || e.target.value.length == 0)) {
+            orderButton.disabled = true
+        } else {
+            orderButton.disabled = false
+        }
+    })
+    purchaseInput.addEventListener('keypress', numbersOnly)
+    purchaseInput.addEventListener('keyup', calculateToEth)
+    orderButton.addEventListener('click', buy, false)
 
-    orderButton
-        .addEventListener('click', buy, false)
     // Connect to web3 & grab data from blockchain
     if (!state.account) {
         blockapi.connect(blockmetadata, async function (newstate) {
@@ -122,6 +192,4 @@
         }, disconnected)
         return false;
     }
-
-
 })();
